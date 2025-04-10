0bf77abe8c2f20c8bc3002f5e89a42cd4018bb71
vaevic <aug120jw@gmail.com>
2025-04-10 20:51:09 +0900

    update bossstage
    메인 머지 한뒤 날려서 다시 제작해서 올림

---
+++ FoodRumble/Source/FoodRumble/GameState/LobbyGameStateBase.h
@@ -0,0 +1,16 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "LobbyGameStateBase.generated.h"
+
+class ALobbyCharacter;
+
+UCLASS()
+class FOODRUMBLE_API ALobbyGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+
+};
